3bfc7a431a15b2b64138e1770f482475613ffdbd
Leah <lifang.moler@gmail.com>
2014-02-08 20:42:46 -0800

    first commit

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :message, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SelfTracking295B::Application.config.secret_key_base = '6c3cbb19a443f36e135c958fd10fae17815e2d16023854bc85c3c56ef2bef9fa4d3ebbb8959cee8f15ebd686f2e906eeb23d73abbb1a5faa264d05cdb7d3614a'
