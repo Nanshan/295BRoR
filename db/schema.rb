33131a4eb84f179ae83d78a6f84849841adc061a
Leah <lifang.moler@gmail.com>
2014-02-10 18:40:56 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140209200516) do
-
+ActiveRecord::Schema.define(version: 20140211023510) do
   create_table "resturants", force: true do |t|
     t.string   "name"
     t.string   "address"
