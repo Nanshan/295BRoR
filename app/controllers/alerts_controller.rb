7242c9eb282934c80bca49d2cc360ef213ae9f8f
EC2 Default User <ec2-user@ip-172-31-19-253.us-west-2.compute.internal>
2014-02-24 09:14:36 -0800

    alert

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -5,7 +5,7 @@ class AlertsController < ApplicationController
      @user_id = params[:userID]
      @events = Location.where(["userID = ?", @user_id]).last(4)
      @alert = "No Alert"
-     if (@evenets.length <3)
+     if (@events.length <3)
         @alert = "No Alert"
      elsif (@events.length == 3)
        @lat = @events[0].latitude
@@ -13,14 +13,14 @@ class AlertsController < ApplicationController
        if (@lat == @events[1].latitude && @lat == @events[2].latitude && @long == @events[1].longitude && @long == @events[2].longitude)
          @alert = "Alert"
        end
-      else
-        if (@lat == @events[1].latitude && @lat == @events[2].latitude && @long == @events[1].longitude && @long == @events[2].longitude && @lat == @events[3].latitude && @long == @events[3].longitude)
+     else
+        if (@events[0].latitude == @events[1].latitude && @events[1].latitude == @events[2].latitude && @events[2].latitude == @events[3].latitude && @events[0].longitude == @events[1].longitude && @events[1].longitude == @events[2].longitude && @events[2].longitude == @events[3].longitude)
         @alert = "No Alert"
         elsif (@events[1].latitude == @events[2].latitude && @events[2].latitude == @events[3].latitude && @events[1].longitude == @events[2].longitude && @events[2].longitude == @events[3].longitude)
            @alert = "Alert"
         else
            @alert = "no alert"
-
+        end
      end
      
      # find 3 most recent events in Location table for this user_id
