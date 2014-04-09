c4b582799c607ec6570505fdb4622ef46923c93f
EC2 Default User <ec2-user@ip-172-31-19-253.us-west-2.compute.internal>
2014-04-08 19:17:21 -0700

    add login and register methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,65 +1,25 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
+  def login
+    @result = "Failure"
+    @user = User.where(["userName = ?", params[:userName]]).first
+    if (@user != nil and @user.password == params[:password])
+      @result = "Success"
+    end
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.create(message: params[:message])
-    @user.save
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+  def register
+    @result = "Failure"
+    @existingUser = User.where(["userName = ?", params[:userName]]).first
+    if (@existingUser != nil)
+      @user = User.new
+      @user.userName = params[:userName]
+      @user.password = params[:password]
+      @user.email = params[:email]
+      if (@user.save)
+        @result = "Success"
       end
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:message)
-    end
 end
