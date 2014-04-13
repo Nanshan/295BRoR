bed26bb36468141b84d4541eff7b221c8751d515
EC2 Default User <ec2-user@ip-172-31-19-253.us-west-2.compute.internal>
2014-04-13 12:27:59 -0700

    fixed location tracking with user id

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,36 +1,13 @@
 class LocationsController < ApplicationController
 
-  # GET /locations
-  # GET /locations.json
+  # exposed for ease of testing only
   def index
     @locations = Location.all        
   end
 
-  # GET /locations/1
-  # GET /locations/1.json
-  def show
-  end
-
-  # POST /locations
-  # POST /locations.json
   def create
-     #@resturant =  Resturant.find_by(latitude:  params[:latitude], longitude: params[:longitude]) 
-     #if @resturant != nil
-         @location = Location.new(latitude: params[:latitude], longitude: params[:longitude], userID: params[:userID]) 
-      
-         @location.save
-         
-      #end
-  end
- 
-  def alert
-     @user_id = params[:userID]
-     # find 3 most recent events in Location table for this user_id
-     # if all 3 have same location
-     # then get the restaurant at that location
-     # and return the alert
-     #
-     # else (not have 3 most recent events having same location which means user has not been there 15 minutes or maybe user left)
-     # return no alert
+    @user_id = User.where(["userName = ?", params[:username]]).first.id
+    @location = Location.new(latitude: params[:latitude], longitude: params[:longitude], userID: @user_id) 
+    @location.save     
   end
 end
