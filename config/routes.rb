23581f35bfebaf4bea8bc3a7c3b04affb03c42a3
EC2 Default User <ec2-user@ip-172-31-19-253.us-west-2.compute.internal>
2014-04-13 12:09:00 -0700

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 SelfTracking295B::Application.routes.draw do
   root to: "places#index" 
   resources :places
-  resources :logins
   resources :histories
   resources :locations 
   resources :alerts
