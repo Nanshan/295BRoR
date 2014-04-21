b90944752be6676cd788060eca527220882832c1
EC2 Default User <ec2-user@ip-172-31-19-253.us-west-2.compute.internal>
2014-04-21 05:58:45 +0000

    added radius search and working search by geo index

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,21 +1,21 @@
 class PlacesController < ApplicationController
+
+  METERS_IN_MILE = 1609.34
+
   # GET /places
   # GET /places.json
   def index
     @type = params[:type].downcase
     @category = params[:category].downcase
-    @lat = params[:lat].to_f.abs
-    @long = params[:long].to_f.abs
-    @places = Place.where("lower(\"locationType\") = ? AND lower(category) = ?", @type, @category).all
-#    if (params[:lat] != nil and params[:long] != nil)
-#      for r in @places
-#        @r_lat = r.latitude.to_f.abs
-#        @r_long = r.longitude.to_f.abs
-#        if ((@lat-@r_lat).abs >= 1) or ((@long-@r_long).abs >= 1)
-#          @places.delete(r)
-#        end
-#      end
-#    end
+    @lat = params[:lat].to_f
+    @long = params[:long].to_f
+    
+    @radius = METERS_IN_MILE * 5
+    if (params[:radius] != nil)
+      @radius = params[:radius].to_i * METERS_IN_MILE
+    end    
+    
+    @places = Place.where("lower(\"locationType\") = ? AND lower(category) = ?", @type, @category).close_to(@lat, @long, @radius)
     if (@places.length > 3)
       @places = @places[0..2]
     end
