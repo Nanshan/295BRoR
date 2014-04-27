6a35e6f2c63fb015758e2c408720b9d18b587b58
EC2 Default User <ec2-user@ip-172-31-19-253.us-west-2.compute.internal>
2014-04-27 19:45:49 +0000

    histories takes the place id

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -13,8 +13,11 @@ class HistoriesController < ApplicationController
   # POST /histories
   # POST /histories.json
   def create
-    @user_id = User.where(["lower(userName) = ?", params[:userName].downcase]).first.id
-    @history = History.new(userId: @user_id, latitude: params[:latitude], longitude: params[:longitude], like: params[:like])
+    @user_id = User.where(["lower(\"userName\") = ?", params[:userName].downcase]).first.id
+    @history = History.new
+    @history.userId = @user_id
+    @history.placeID = params[:placeID]
+    @history.like = params[:like]
     @history.save
   end
 end
