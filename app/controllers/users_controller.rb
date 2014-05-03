95f81e0172722c69bfe13d259419389da6f0ccb9
EC2 Default User <ec2-user@ip-172-31-19-253.us-west-2.compute.internal>
2014-05-03 19:48:39 +0000

    basic similarity metirc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,4 +39,41 @@ class UsersController < ApplicationController
     end
   end
 
+  def calc_similarity(user1, user2)
+    similarity = 1.0
+    similarity_mod = 0.2
+
+    # age
+    if (user1.date_of_birth != nil and user2.date_of_birth != nil)
+      age_diff = (user1.date_of_birth.year - user2.date_of_birth.year).abs
+      if (age_diff >= 20)
+        similarity -= similarity_mod
+      elsif (age_diff <= 9)
+        similarity += similarity_mod
+      end
+    end
+
+    # gender
+    if (user1.gender != nil and user2.gender != nil and user1.gender == user2.gender)
+      similarity += similarity_mod
+    end
+
+    # marital status
+    if (user1.marital_status != nil and user2.marital_status != nil and user1.marital_status == user2.marital_status)
+      similarity += similarity_mod
+    end
+
+    # parental status
+    if (user1.has_kids != nil and user2.has_kids != nil and user1.has_kids == user2.has_kids)
+      similarity += similarity_mod
+    end
+
+    # home country
+    if (user1.hometown_country != nil and user2.hometown_country != nil and user1.hometown_country == user2.hometown_country)
+      similarity += similarity_mod
+    end
+
+    return similarity
+  end
+
 end
