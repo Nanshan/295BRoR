e8a38738244ca70f1bc7ab92015a1b5502164b7d
EC2 Default User <ec2-user@ip-172-31-19-253.us-west-2.compute.internal>
2014-05-05 03:28:51 +0000

    recommendations based on likes and similarities

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,7 +14,22 @@ class PlacesController < ApplicationController
       @radius = params[:radius].to_i * METERS_IN_MILE
     end    
     
+    @user_id = User.where(["lower(\"userName\") = ?", params[:username].downcase]).first.id
+
     @places = Place.where("lower(\"locationType\") = ? AND lower(category) = ?", @type, @category).close_to(@lat, @long, @radius)
+    @placeHash = Hash.new
+
+    @places.each do |place|
+      score = 0
+      @likes = History.where("\"placeID\" = ?", place.id)
+      @likes.each do |like|
+        score += like.like * Similarity.where("user1_id = ? AND user2_id = ?", @user_id, like.userId.to_i).take.similarity
+      end
+      @placeHash[place] = score
+    end
+
+    @places.sort {|x, y| @placeHash[x] <=> @placeHash[y]}
+
     if (@places.length > 3)
       @places = @places[0..2]
     end
